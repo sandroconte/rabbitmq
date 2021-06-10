dcf585913e8f4513ddd8ceb34eb3e8b66f44423c
sandro.conte <sandroconte.dev@gmail.com>
2021-06-10 16:13:26 +0200

    Update task.sh

--- task.sh
+++ task.sh
@@ -5,7 +5,7 @@ for TASK in $(rabbitmqctl list_queues -q name messages -p pharmanow_vhost)
 do
 	if [ $((i%2)) -ne 0 ]
 	then
-		echo $TASK
+		echo $(rabbitmqadmin get queue=$TASK -u pharmanow -p Ph4rm4-rabbitmq -V pharmanow_vhost)
 	fi
 	i=$((i+1))
 done
