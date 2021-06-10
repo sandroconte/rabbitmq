188f8eff99ec3f81432f66e5fb94c8ff45aabe7d
sandro.conte <sandroconte.dev@gmail.com>
2021-06-10 16:08:17 +0200

    Update task.sh

--- task.sh
+++ task.sh
@@ -3,9 +3,9 @@
 index=1
 for TASK in $(rabbitmqctl list_queues -q name messages -p pharmanow_vhost)
 do
-	if [ $((a%2)) -ne 0 ]
+	if [ $((index%2)) -ne 0 ]
 	then
 		echo $TASK
 	fi
-	
+	index++
 done
